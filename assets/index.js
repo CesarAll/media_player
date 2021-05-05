4c900a18dd8ddffac2d6e3004ae8c4baa8eefac7
cesarAll <cesaralcantara.rmx@gmail.com>
2021-05-05 16:55:56 -0500

    creando los modulos en js

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,5 @@
+import MediaPlayer  from './mediaPlayer'
 
-
-function MediaPlayer (config){
-    this.media = config.element;
-}
-MediaPlayer.prototype.play = function(){
-    this.media.play()
-}
-MediaPlayer.prototype.pause = function(){
-    this.media.pause()
-}
-MediaPlayer.prototype.toggleplay = function() {
-    if(this.media.paused){
-        this.play()
-    } else {
-        this.pause()
-    }
-}
 const video = document.querySelector("video")
 const player = new MediaPlayer({element:video})
 
